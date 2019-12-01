1e1804585414af2f612ffe19c629f4a52cdecd48
harkiramadhan <harkiramadhan@gmail.com>
2019-12-02 02:56:55 +0700

    php mailer for registration info

--- ppdb/application/controllers/Ppdb.php
+++ ppdb/application/controllers/Ppdb.php
@@ -44,10 +44,36 @@ class Ppdb extends CI_Controller{
             $this->db->insert('csiswa', $data);
 
             if($this->db->affected_rows() > 0){
-                $success = "Silahkan Cek Email <b>".$email."</b> Untuk Melakukan Login";
-                $this->session->set_flashdata('sukses', $success);
-                redirect('ppdb');
-                print_r($this->input->post());
+                $this->load->library('PHPMailer_lib');
+                $mail = $this->phpmailer_lib->load();
+
+                $mail->isSMTP();
+                $mail->Host     = 'mail.alhikmahmp.sch.id';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'ppdb@alhikmahmp.sch.id';
+                $mail->Password = 's1mpaud3v';
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port     = 465;
+
+                $mail->setFrom('ppdb@alhikmahmp.sch.id', 'PPDB Al Hikmah');
+                $mail->addReplyTo('ppdb@alhikmahmp.sch.id', 'HarkiRamadhan');
+                
+                $mail->addAddress($this->input->post('email', TRUE));
+                
+                $mail->Subject = 'PPDB Online SDIT Al Hikmah';
+                
+                $mail->isHTML(true);
+                $mailContent = "<h1>".$this->input->post('nama', TRUE)."</h1>
+                    <p>This is a test email sending using SMTP mail server with PHPMailer.</p>";
+                $mail->Body = $mailContent;
+
+                if($mail->send()){
+                    $success = "Silahkan Cek Email <b>".$email."</b> Untuk Melakukan Login";
+                    $this->session->set_flashdata('sukses', $success);
+                    redirect('ppdb');
+                }else{
+                    echo $mail->ErrorInfo;
+                }
             }else{
 
             }
