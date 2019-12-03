2e7f99b537ef6ff9b802b9d51901e96e17b047b5
harkiramadhan <harkiramadhan@gmail.com>
2019-12-04 00:01:34 +0700

    biodata ortu

--- ppdb/application/views/inner/bio_ortu.php
+++ ppdb/application/views/inner/bio_ortu.php
@@ -7,4 +7,95 @@
 </div>
 
 <!-- Page content -->
-<div class="container-fluid mt--4">
+<div class="container-fluid mt--5">
+    <div class="col-xl-12">
+        <div class="nav-wrapper">
+            <ul class="nav nav-pills nav-fill flex-column flex-md-row" id="tabs-icons-text" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true">Biodata Ayah</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false">Biodata Ibu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab" href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3" aria-selected="false">Biodata Wali</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="tabs-icons-text-1" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">Nama Lengkap <small class="text-warning"><b>*) Tanpa Gelar</b></small></label>
+                                    <input type="text" class="form-control form-control-alternative form-control-sm" placeholder="Nama Lengkap Ayah" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Nomor Induk Kependudukan <small class="text-warning">*</small></label>
+                                    <input type="number" class="form-control form-control-alternative form-control-sm" placeholder="Nomor Induk Kependudukan" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Tempat Lahir <small class="text-warning">*</small></label>
+                                    <input type="text" class="form-control form-control-alternative form-control-sm" placeholder="Tempat Lahir" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Tanggal Lahir <small class="text-warning">*</small></label>
+                                    <input type="date" class="form-control form-control-alternative form-control-sm" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Tempat Tinggal <small class="text-warning">*</small></label>
+                                    <select name="" id="" class="form-control form-control-alternative form-control-sm" required>
+                                        <option value="" selected disabled>- Pilih Tempat Tinggal -</option>
+                                        <option value="">Rumah Sendiri</option>
+                                        <option value="">Rumah Orang Tua</option>
+                                        <option value="">Mengontrak</option>
+                                        <option value="">Dinas</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">Pendidikan Terakhir <small class="text-warning">*</small></label>
+                                    <select name="" id="" class="form-control form-control-alternative form-control-sm" required>
+                                        <option value="" selected disabled>- Pilih Pendidikan Terakhir -</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Gelar Pendidikan</label>
+                                    <input type="text" class="form-control form-control-alternative form-control-sm" placeholder="Gelar Pendidikan">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Pekerjaan <small class="text-warning">*</small></label>
+                                    <select name="" id="" class="form-control form-control-alternative form-control-sm" required>
+                                        <option value="" selected disabled>- Pilih Pekerjaan -</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Nama & Alamat Pekerjaan</label>
+                                    <textarea name="" id="" cols="30" rows="5" class="form-control form-control-alternative form-control-sm"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Email</label>
+                                    <input type="email" class="form-control form-control-alternative form-control-sm" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Penghasilan Perbulan (Rp.) <small class="text-warning">*</small></label>
+                                    <select name="" id="" class="form-control form-control-alternative form-control-sm" required>
+                                        <option value="" selected disabled>- Pilih Penghasilan Perbulan -</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
+                        <p class="description">Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+                    </div>
+                    <div class="tab-pane fade" id="tabs-icons-text-3" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
+                        <p class="description">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
