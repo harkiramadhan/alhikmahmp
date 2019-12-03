56f669bfa212e506d0b3dd21ed22b92aca50764c
harkiramadhan <harkiramadhan@gmail.com>
2019-12-03 16:00:14 +0700

    metode pembayaran, konfirmasi pembayaran

--- ppdb/application/views/inner/dashboard.php
+++ ppdb/application/views/inner/dashboard.php
@@ -7,9 +7,8 @@
 </div>
 <!-- Page content -->
 <div class="container-fluid mt--4">
-    <div class="row">
-        <div class="col-xl-12 order-xl-1">
-            <!-- Card stats -->
+    <div class="row justify-content-center">
+        <div class="col-xl-12">
             <div class="row">
                 <div class="col-xl-3">
                     <div class="card shadow card-stats mb-4 text-center">
@@ -40,6 +39,36 @@
                             <h3 class="m-0"><strong><?= $anak->tl.", ".date('d-m-Y', strtotime($anak->tgl_lahir)) ?></strong></h3>  
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="card shadow card-stats mb-4 text-center">
+                <div class="card-header bg-secondary">
+                    <h5 class="card-title text-uppercase text-muted m-0">Metode Pembayaran</h5> 
+                </div>
+                <div class="card-body"> 
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <select name="" id="" class="form-control form-control-alternative form-control-sm">
+                                <option value="" selected disabled>- Pilih Metode Pembayaran -</option>
+                                <option value="">Transfer</option>
+                                <option value="">Bayar Di Sekolah</option>
+                            </select>
+                        </div>
+                        <div class="text-right">
+                            <button class="btn btn-sm btn-info">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="card shadow card-stats mb-4 text-center">
+                <div class="card-header bg-secondary">
+                    <h5 class="card-title text-uppercase text-muted m-0">Konfirmasi Pembayaran</h5> 
+                </div>
+                <div class="card-body"> 
                 </div>
             </div>
         </div>
