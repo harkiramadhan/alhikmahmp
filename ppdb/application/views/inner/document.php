ef250a697374da5a151038928af194fdb68409be
harkiramadhan <harkiramadhan@gmail.com>
2019-12-08 23:59:30 +0700

    document upload layout

--- ppdb/application/views/inner/document.php
+++ ppdb/application/views/inner/document.php
@@ -8,3 +8,35 @@
 
 <!-- Page content -->
 <div class="container-fluid mt--4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card shadow">
+                <div class="card-header">
+                    <h3 class="m-0">Ktp Kedua Orang Tua</h3>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card shadow">
+                <div class="card-header">
+                    <h3 class="m-0">Kartu Keluarga</h3>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card shadow">
+                <div class="card-header">
+                    <h3 class="m-0">Akta Kelahiran Anak</h3>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
