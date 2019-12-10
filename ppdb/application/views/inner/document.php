5767a1e08650bb8e6bd144affc770fc80addb415
harkiramadhan <harkiramadhan@gmail.com>
2019-12-10 11:48:12 +0700

    upload document view

--- ppdb/application/views/inner/document.php
+++ ppdb/application/views/inner/document.php
@@ -9,43 +9,76 @@
 <!-- Page content -->
 <div class="container-fluid mt--4">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-xl-6 mt-sm-3">
             <div class="card shadow">
-                <div class="card-header">
-                    <h3 class="m-0">Ktp Kedua Orang Tua</h3>
+                <div class="card-header bg-secondary">
+                    <h3 class="m-0">Foto Calon Siswa</h3>
                 </div>
+                <form action="<?= site_url('document/simpan') ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="jenis" value="anak">
                 <div class="card-body">
-                    
+                    <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source" onchange="previewImage();" accept=".png, .jpg, .jpeg" >
+                    <label for="imageUpload" class="m-0"></label>
+                    <img id="image-preview" class="rounded d-none" alt="image preview" style="width:100%;"/>    
                 </div>
-                <div class="card-footer">
-                
+                <div class="card-footer bg-secondary">
+                    <button type="submit" class="btn btn-sm btn-success btn-block">Simpan</button>
                 </div>
+                </form>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-xl-6 mt-sm-3">
             <div class="card shadow">
-                <div class="card-header">
-                    <h3 class="m-0">Kartu Keluarga</h3>
+                <div class="card-header bg-secondary">
+                    <h3 class="m-0">Scan Ktp Kedua Orang Tua</h3>
                 </div>
+                <form action="<?= site_url('document/simpan') ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="jenis" value="ktp">
                 <div class="card-body">
-                    
+                    <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source2" onchange="previewImage2();" accept=".png, .jpg, .jpeg" >
+                    <label for="imageUpload" class="m-0"></label>
+                    <img id="image-preview2" class="rounded d-none" alt="image preview" style="width:100%;"/>    
                 </div>
-                <div class="card-footer">
-                
+                <div class="card-footer bg-secondary">
+                    <button type="submit" class="btn btn-sm btn-success btn-block">Simpan</button>
                 </div>
+                </form>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-xl-6 mt-sm-3 mt-xl-4">
             <div class="card shadow">
-                <div class="card-header">
-                    <h3 class="m-0">Akta Kelahiran Anak</h3>
+                <div class="card-header bg-secondary">
+                    <h3 class="m-0">Scan Kartu Keluarga</h3>
                 </div>
+                <form action="<?= site_url('document/simpan') ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="jenis" value="kk">
                 <div class="card-body">
-                    
+                    <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source3" onchange="previewImage3();" accept=".png, .jpg, .jpeg" >
+                    <label for="imageUpload" class="m-0"></label>
+                    <img id="image-preview3" class="rounded d-none" alt="image preview" style="width:100%;"/>    
                 </div>
-                <div class="card-footer">
-                
+                <div class="card-footer bg-secondary">
+                    <button type="submit" class="btn btn-sm btn-success btn-block">Simpan</button>
                 </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-xl-6 mt-sm-3 mt-xl-4">
+            <div class="card shadow">
+                <div class="card-header bg-secondary">
+                    <h3 class="m-0">Scan Akta Kelahiran Anak</h3>
+                </div>
+                <form action="<?= site_url('document/simpan') ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="jenis" value="akta">
+                <div class="card-body">
+                    <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source4" onchange="previewImage4();" accept=".png, .jpg, .jpeg" >
+                    <label for="imageUpload" class="m-0"></label>
+                    <img id="image-preview4" class="rounded d-none" alt="image preview" style="width:100%;"/>    
+                </div>
+                <div class="card-footer bg-secondary">
+                    <button type="submit" class="btn btn-sm btn-success btn-block">Simpan</button>
+                </div>
+                </form>
             </div>
         </div>
     </div>
--- ppdb/assets/inner/js/style.js
+++ ppdb/assets/inner/js/style.js
@@ -71,6 +71,9 @@ $(document).ready(function() {
 });
 
 function previewImage() {
+    var element = document.getElementById("image-preview");
+        element.classList.remove("d-none");
+
     document.getElementById("image-preview").style.display = "block";
     var oFReader = new FileReader();
      oFReader.readAsDataURL(document.getElementById("image-source").files[0]);
@@ -78,8 +81,43 @@ function previewImage() {
     oFReader.onload = function(oFREvent) {
       document.getElementById("image-preview").src = oFREvent.target.result;
     };
-  };
+};
 
-  
+function previewImage2() {
+    var element = document.getElementById("image-preview2");
+        element.classList.remove("d-none");
 
-  
+    document.getElementById("image-preview2").style.display = "block";
+    var oFReader = new FileReader();
+     oFReader.readAsDataURL(document.getElementById("image-source2").files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      document.getElementById("image-preview2").src = oFREvent.target.result;
+    };
+};
+
+function previewImage3() {
+    var element = document.getElementById("image-preview3");
+        element.classList.remove("d-none");
+
+    document.getElementById("image-preview3").style.display = "block";
+    var oFReader = new FileReader();
+     oFReader.readAsDataURL(document.getElementById("image-source3").files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      document.getElementById("image-preview3").src = oFREvent.target.result;
+    };
+};
+
+function previewImage4() {
+    var element = document.getElementById("image-preview4");
+        element.classList.remove("d-none");
+
+    document.getElementById("image-preview4").style.display = "block";
+    var oFReader = new FileReader();
+     oFReader.readAsDataURL(document.getElementById("image-source4").files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      document.getElementById("image-preview4").src = oFREvent.target.result;
+    };
+};
