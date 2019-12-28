ddf81614c9a62919930c8a96b3aaa1452f95e540
harkiramadhan <harkiramadhan@gmail.com>
2019-12-28 20:55:17 +0700

    row edit

--- ppdb/application/views/inner/daftar_csiswa.php
+++ ppdb/application/views/inner/daftar_csiswa.php
@@ -7,32 +7,34 @@
 </div>
 <!-- Page content -->
 <div class="container-fluid mt--5">
-    <div class="col-xl-12 mt-3">
-        <div class="card">
-            <div class="form-group-sm">
-                <input type="text" class="form-control form-control-md form-control-alternative" placeholder="Cari ..." id="search">
+    <div class="row justify-content-center">
+        <div class="col-xl-12 mt-3">
+            <div class="card">
+                <div class="form-group-sm">
+                    <input type="text" class="form-control form-control-md form-control-alternative" placeholder="Cari ..." id="search">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xl-12 mt-2">
-        <div class="card shadow">
-            <div class="table-responsive">
-                <table class="table table align-items-center table-flush table-hover table-sm" id="csiswa">
-                    <thead class="thead-light">
-                        <tr>
-                            <th width="10px;">No</th>
-                            <th width="10px;">Noujian</th>
-                            <th>Nama</th>
-                            <th width="10px;">L/P</th>
-                            <th width="10px;">Asal Sekolah</th>
-                            <th width="10px;" class="text-center">Status</th>
-                            <th width="10px;" class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                </table>
+        <div class="col-xl-12 mt-2">
+            <div class="card shadow">
+                <div class="table-responsive">
+                    <table class="table table align-items-center table-flush table-hover table-sm" id="csiswa">
+                        <thead class="thead-light">
+                            <tr>
+                                <th width="10px;">No</th>
+                                <th width="10px;">Noujian</th>
+                                <th>Nama</th>
+                                <th width="10px;">L/P</th>
+                                <th width="10px;">Asal Sekolah</th>
+                                <th width="10px;" class="text-center">Status</th>
+                                <th width="10px;" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
