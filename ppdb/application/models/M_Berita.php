75a612de15fc6a5263f59d3817d6bab7afb5ccdb
harkiramadhan <harkiramadhan@gmail.com>
2020-01-28 11:55:23 +0700

    update

--- ppdb/application/models/M_Berita.php
+++ ppdb/application/models/M_Berita.php
@@ -7,4 +7,12 @@ class M_Berita extends CI_Model{
         $this->db->order_by('id', "DESC");
         return $this->db->get();
     }
+
+    function get_LabelByBerita($idberita){
+        $this->db->select('label.label');
+        $this->db->from('label_berita');
+        $this->db->join('berita', "label_berita.id_berita = berita.id");
+        $this->db->where(['label_berita.id_berita'=>$idberita]);
+        return $this->db->get();
+    }
 }
