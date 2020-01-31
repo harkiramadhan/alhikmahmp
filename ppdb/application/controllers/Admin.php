ddf4a6a52ea9adf92d868cafecc005d0b21a092e
harkiramadhan <harkiramadhan@gmail.com>
2020-01-31 21:21:55 +0700

    update

--- ppdb/application/controllers/Admin.php
+++ ppdb/application/controllers/Admin.php
@@ -8,11 +8,24 @@ class Admin extends CI_Controller{
         $username = $this->input->post('username', TRUE);
         $password = md5($this->input->post('password', TRUE));
 
-        echo $username; 
-        echo $password;
+        $cek = $this->M_Admin->cek_userAdmin($username, $password);
+        if($cek->num_rows() > 0){
+            $data = $cek->row();
+
+            $this->session->set_userdata('masuk', TRUE);
+            $this->session->set_userdata('email', $data->email);
+            $this->session->set_userdata('role', $data->role);
+
+            redirect('dashboard');
+        }else{
+            $this->session->set_flashdata('error', "Username Atau Password Salah");
+            redirect($_SERVER['HTTP_REFERER']);
+        }
     }
 
     function logout(){
-        
+        $this->session->sess_destroy();
+        $url = base_url();
+        redirect($url);
     }
 }
