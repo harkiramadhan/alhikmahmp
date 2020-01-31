f914693b196e13c038616d5492283307ecafa314
harkiramadhan <harkiramadhan@gmail.com>
2020-01-31 21:23:24 +0700

    update

--- ppdb/application/controllers/Admin.php
+++ ppdb/application/controllers/Admin.php
@@ -1,5 +1,10 @@
 <?php
 class Admin extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        $this->load->model('M_Admin');
+    }
+
     function index(){
         $this->load->view('admin/login');
     }
--- ppdb/application/models/M_Admin.php
+++ ppdb/application/models/M_Admin.php
@@ -6,4 +6,11 @@ class M_Admin extends CI_Model{
         $this->db->where(['email'=>$email, 'password'=>md5($password)]);
         return $this->db->get();
     }
+
+    function cek_userAdmin($username, $password){
+        $this->db->select('*');
+        $this->db->from('admin');
+        $this->db->where(['email'=>$username, 'password'=>md5($password)]);
+        return $this->db->get();
+    }
 }
