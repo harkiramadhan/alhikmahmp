fb938d53b8a5e2eb0b6696aca88d448c5d80a126
harkiramadhan <harkiramadhan@gmail.com>
2020-01-31 22:31:58 +0700

    update

--- ppdb/application/views/admin/profile.php
+++ ppdb/application/views/admin/profile.php
@@ -7,3 +7,45 @@
 </div>
 <!-- Page content -->
 <div class="container-fluid mt--5">
+    <div class="row">
+        <div class="col-lg-12 col-md-10">
+            <h1 class="text-white m-0"><strong>Profile</strong></h1>
+        </div>
+        <div class="col-xl-12 mb-5 mb-xl-0 mt-5">
+            <div class="card shadow bg-secondary">
+                <div class="row container">
+                    <div class="col-md-6 mt-2">
+                        <div class="form-group">
+                            <label for="">Jenjang Sekolah</label>
+                            <select name="jenjang" id="" class="form-control form-control-alternative form-control-sm">
+                                <option value="">- Pilih Jenjang -</option>
+                                <option value="TK">TK</option>
+                                <option value="SD">SD</option>
+                                <option value="SMP">SMP</option>
+                                <option value="SMA">SMA</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Nama Sekolah</label>
+                            <input type="text" name="nama" id="" class="form-control form-control-alternative form-control-sm" placeholder="Nama Sekolah">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Sejarah Sekolah</label>
+                            <textarea name="sejarah" id="" cols="30" rows="10" class="form-control form-control-alternative form-control-sm" placeholder="Sejarah Sekolah"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Visi Sekolah</label>
+                            <textarea name="visi" id="" cols="30" rows="10" class="form-control form-control-alternative form-control-sm" placeholder="Visi Sekolah"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Misi Sekolah</label>
+                            <textarea name="misi" id="" cols="30" rows="10" class="form-control form-control-alternative form-control-sm" placeholder="Misi Sekolah"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mt-2">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
