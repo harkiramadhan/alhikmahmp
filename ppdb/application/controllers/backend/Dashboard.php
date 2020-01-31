b4641a750ea00841a2a45505b71e1c896fa636d6
harkiramadhan <harkiramadhan@gmail.com>
2020-01-31 21:42:04 +0700

    update

--- ppdb/application/controllers/backend/Dashboard.php
+++ ppdb/application/controllers/backend/Dashboard.php
@@ -1,6 +1,10 @@
 <?php
 class Dashboard extends CI_Controller{
     function index(){
-        echo "Dashboard Admin";
+        $data['title'] = "Dashboard Admin Al Hikmah";
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/dashboard');
+        $this->load->view('admin/footer');
     }
 }
