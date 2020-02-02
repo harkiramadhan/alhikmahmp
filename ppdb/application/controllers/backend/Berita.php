c2abbb7508493ac413975c3cdfa55a632ad8c681
harkiramadhan <harkiramadhan@gmail.com>
2020-02-02 19:38:29 +0700

    update

---
+++ ppdb/application/controllers/backend/Berita.php
@@ -0,0 +1,27 @@
+<?php
+class Berita extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        $this->load->model('M_Admin');
+        $this->load->model('M_Berita');
+        $this->load->model('M_Label');
+        if($this->session->userdata('masuk') != TRUE){
+            $url = base_url();
+            redirect($url);
+        }
+    }
+
+    private function session($jenis){
+        $sess = $this->session->userdata($jenis);
+        return $sess;
+    }
+
+    function index(){
+        $data['title'] = "Dashboard Admin Al Hikmah";
+        $data['nama'] = $this->session('email');
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/berita');
+        $this->load->view('admin/footer');
+    }
+}
---
+++ ppdb/application/views/admin/berita.php
@@ -0,0 +1,96 @@
+<!-- Header -->
+<div class="header bg-gradient-warning pb-8">
+    <div class="container-fluid">
+        <div class="header-body">
+        </div>
+    </div>
+</div>
+<!-- Page content -->
+<div class="container-fluid mt--3">
+    <div class="row">
+        <div class="col">
+          <div class="card shadow">
+            <div class="tab-content" id="myTabContent">
+
+              <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                <div class="col card-header d-md-inline-block bg-transparent border-0">
+                  <div class="row align-items-center">
+                    <div class="col-xl-10 mt-2">
+                      <h3 class="mb-0">Daftar Berita </h3> 
+                    </div>
+                    <div class="col-xl-2 mt-2">
+                        <button class="btn btn-sm btn-success btn-block" data-toggle="modal" data-target="#tambah">Tambah Berita</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush table-sm">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col" width="10">No</th>
+                        <th scope="col">Label</th>
+                        <th scope="col" class="col-12">Judul Berita</th>
+                        <th scope="col" class="text-center">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody class="myTable">
+                        <tr>
+                            
+                        </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <!-- Modal Tambah Label-->
+    <div class="modal fade" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Label</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="<?= site_url('backend/label/simpan') ?>" method="POST">
+                <input type="hidden" name="jenis" value="tambah">
+                <div class="modal-body bg-secondary">
+                    <div class="col-xl-12 text-center">
+                        <span class="badge badge-pill badge-default">Default</span>
+                        <span class="badge badge-pill badge-primary">Primary</span>
+                        <span class="badge badge-pill badge-secondary">Secondary</span>
+                        <span class="badge badge-pill badge-info">Info</span>
+                        <span class="badge badge-pill badge-success">Success</span>
+                        <span class="badge badge-pill badge-danger">Danger</span>
+                        <span class="badge badge-pill badge-warning">Warning</span>
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="">Badge</label>
+                        <select name="badge" id="" class="form-control form-control-sm form-control-alternative">
+                            <option value="">- Pilih Badge -</option>
+                            <option value="badge-default">Default</option>
+                            <option value="badge-primary">Primary</option>
+                            <option value="badge-secondary">Secondary</option>
+                            <option value="badge-info">Info</option>
+                            <option value="badge-success">Success</option>
+                            <option value="badge-danger">Danger</option>
+                            <option value="badge-warning">Warning</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Label</label>
+                        <input type="text" name="label" id="" class="form-control form-control-alternative form-control-sm" placeholder="Label">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
