9f968988f3ca283bbadd6c72ab9457eb7db9335e
harkiramadhan <harkiramadhan@gmail.com>
2020-02-02 15:16:49 +0700

    update

--- ppdb/application/views/admin/label.php
+++ ppdb/application/views/admin/label.php
@@ -58,13 +58,18 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Label</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="form-group">
+                    <label for="">Badge</label>
+                    <select name="badge" id="" class="form-control form-control-sm form-control-alternative">
+                        <option value="">- Pilih Badge -</option>
+                    </select>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Tutup</button>
