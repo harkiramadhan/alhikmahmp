696ecb528b8e1f40c0258eed9113415208913615
harkiramadhan <harkiramadhan@gmail.com>
2020-02-02 14:11:04 +0700

    update

--- ppdb/application/controllers/backend/Label.php
+++ ppdb/application/controllers/backend/Label.php
@@ -1,6 +1,26 @@
 <?php
 class Label extends CI_Controller{
-    function index(){
+    function __construct(){
+        parent::__construct();
+        $this->load->model('M_Admin');
         
+        if($this->session->userdata('masuk') != TRUE){
+            $url = base_url();
+            redirect($url);
+        }
+    }
+
+    private function session($jenis){
+        $sess = $this->session->userdata($jenis);
+        return $sess;
+    }
+
+    function index(){
+        $data['title'] = "Dashboard Admin Al Hikmah";
+        $data['nama'] = $this->session('email');
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/label');
+        $this->load->view('admin/footer');
     }
 }
