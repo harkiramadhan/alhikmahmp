d588fb62d215552106b7da2a59e774d7ca0b6188
harkiramadhan <harkiramadhan@gmail.com>
2020-02-02 14:14:57 +0700

    update

---
+++ ppdb/application/views/admin/label.php
@@ -0,0 +1,44 @@
+<!-- Header -->
+<div class="header bg-gradient-warning pb-8">
+    <div class="container-fluid">
+        <div class="header-body">
+        </div>
+    </div>
+</div>
+<!-- Page content -->
+<div class="container-fluid mt--5">
+    <div class="row">
+        <div class="col">
+          <div class="card shadow">
+            <div class="tab-content" id="myTabContent">
+
+              <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                <div class="col card-header d-md-inline-block bg-transparent border-0">
+                  <div class="row align-items-center">
+                    <div class="col">
+                      <h3 class="mb-0">Daftar Label </h3> 
+                    </div>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col" width="10">No</th>
+                        <th scope="col">Badge</th>
+                        <th scope="col" class="col-12">Label</th>
+                        <th scope="col" class="text-center">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody class="myTable">
+                        <tr>
+                        
+                        </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
+      </div>
