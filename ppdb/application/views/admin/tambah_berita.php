38361fd52f2c56ff0ea895c781d98c9e623fedcb
harkiramadhan <harkiramadhan@gmail.com>
2020-02-16 23:47:39 +0700

    update

--- ppdb/application/views/admin/tambah_berita.php
+++ ppdb/application/views/admin/tambah_berita.php
@@ -19,14 +19,14 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="">Judul Berita</label>
-                                <input type="text" name="judul" id="" class="form-control form-control-alternative form-control-sm">
+                                <label for="">Judul Berita <small class="text-warning">*</small></label>
+                                <input type="text" name="judul" id="" class="form-control form-control-alternative form-control-sm" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="">Status Berita</label>
-                                <select name="status" id="" class="form-control form-control-alternative form-control-sm">
+                                <label for="">Status Berita <small class="text-warning">*</small></label>
+                                <select name="status" id="" class="form-control form-control-alternative form-control-sm" required>
                                     <option value="" selected disabled>- Pilih Status Berita -</option>
                                     <option value="published">Published</option>
                                     <option value="draft">Draft</option>
@@ -34,13 +34,13 @@
                             </div>
                         </div>
                         <div class="col-xl-12">
-                            <label for="">Label Berita</label>
+                            <label for="">Label Berita <small class="text-warning">*</small></label>
                             <div class="col-md-12 m-1">
                                  <?php 
                                 $noo=1;
                                 foreach($label->result() as $l){ ?>
                                 <div class="custom-control custom-control-inline custom-checkbox mb-3">
-                                    <input class="custom-control-input" id="customCheck<?= $noo ?>" type="checkbox" value="<?= $l->id ?>" name="idl[]">
+                                    <input class="custom-control-input" id="customCheck<?= $noo ?>" type="checkbox" value="<?= $l->id ?>" name="idl[]" required>
                                     <label class="custom-control-label" for="customCheck<?= $noo ?>"><span class="badge <?= $l->badge ?> mr-1"><?= $l->label ?></span></label>
                                 </div>
                                 <?php 
@@ -49,13 +49,13 @@
                             </div>
                         </div>
                         <div class="col-xl-12">
-                            <label for="">Upload Gambar</label>
-                            <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source" onchange="previewImage();" accept=".png, .jpg, .jpeg" >
+                            <label for="">Upload Gambar <small class="text-warning">*</small></label>
+                            <input class="btn btn-sm btn-outline-primary btn-block" type="file" name="img" id="image-source" onchange="previewImage();" accept=".png, .jpg, .jpeg" required >
                             <label for="imageUpload" class="m-0"></label>
                             <img id="image-preview" class="rounded d-none" alt="image preview" style="width:100%;"/>   
                         </div>
                         <div class="col-xl-12">
-                            <label for="">Berita</label>
+                            <label for="">Konten Berita <small class="text-warning">*</small></label>
                             <div id="editorBerita" style="height: auto;"></div>
                             <textarea name="konten" style="display:none" id="desc_berita"></textarea>
                         </div>
